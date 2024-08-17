33692c96ec479c3363933c4bd098506cc388c906
shahanaMubarak <121827465+shahanaMubarak@users.noreply.github.com>
2024-08-17 20:20:22 +0400

    Update App.jsx

--- fooddelAdmin/src/App.jsx
+++ fooddelAdmin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const url = 'https://foodappbackend-9xns.onrender.com';  // Ensure this matches your backend URL
+  const url = 'https://fooddelapp-back-end.onrender.com';  // Ensure this matches your backend URL
 
   return (
     <div>
